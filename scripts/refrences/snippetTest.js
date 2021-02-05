90d498998e23bcc9c0452b9108f30b82d0543be5
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2021-02-06 01:57:32 +0530

    u

---
+++ scripts/refrences/snippetTest.js
@@ -0,0 +1,17 @@
+console.log("test.js is running!");
+
+
+//this is a working sample of a snippet 
+//https://www.youtube.com/watch?v=DloHqUfPbJc&t=322s
+//javascript:(function(){var script = document.createElement('script');script.src = 'https://shiffman.net/a2z/js/bookmarklet.js' + new Date().getTime();document.body.appendChild(script);})();
+
+// function saveTabData(tab) {
+// 	if (tab.incognito) {
+// 	  return;
+// 	} else {
+// 	  chrome.storage.local.set({data: tab.url});
+// 	}
+//   }
+
+// https://developer.chrome.com/extensions/samples#search:contextMenus
+
